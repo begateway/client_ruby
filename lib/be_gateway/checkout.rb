20f6bbbffef9dcbbb210e105b3f10e82aa0fd426
Mac Shifford <shiroginne@gmail.com>
2013-09-30 12:31:20 +0300

    Add Checkout class to work with checkout page. Split client class to connection and client.

---
+++ lib/be_gateway/checkout.rb
@@ -0,0 +1,15 @@
+module BeGateway
+  class Checkout
+    include Connection
+    
+    def get_token(params)
+      response = post "/api/checkouts", { request: params }
+      make_response(response)
+    end
+    
+    def query(token)
+      response = get "/api/checkouts", { token: token }
+      make_response(response)      
+    end
+  end
+end
--- lib/be_gateway/client.rb
+++ lib/be_gateway/client.rb
@@ -3,18 +3,7 @@ require 'faraday_middleware'
 
 module BeGateway
   class Client
-    extend Forwardable
-    def_delegators :connection, :headers, :headers=
-    
-    cattr_accessor :rack_app, :stub_app, :proxy
-    attr_reader :options
-
-    def initialize(params)
-      @login = params[:shop_id]
-      @password = params[:secret_key]
-      @url = params[:url]
-      @options = params[:options]
-    end
+    include Connection
     
     def authorize(params)
       response = post "/transactions/authorizations", { request: params }
@@ -59,47 +48,6 @@ module BeGateway
 
     def notification(params)
       Response.new(params)
-    end
-
-    private
-
-    attr_reader :login, :password, :url
-
-    def make_response(response)
-      response.status == 200 ? Response.new(response.body) : ErrorResponse.new(response.body)
-    end
-
-    def post(path, params)
-      send_request('post', path, params)
-    end
-
-    def get(path)
-      send_request('get', path)
-    end
-
-    def send_request(method, path, params = nil)
-      begin
-        connection.public_send(method, path, params)
-      rescue Faraday::Error::ClientError=> e
-        ErrorResponse.new({ body: { status: 503, message: 'Gateway is temporarily unavailable', error: e.message } })
-      end
-    end
-
-    def connection
-      @connection ||= Faraday.new(url, options = {}) do |conn|
-        conn.request :json
-        conn.request :basic_auth, login, password
-
-        conn.response :json
-
-        conn.proxy(proxy) if proxy
-
-        conn.adapter :test, stub_app if stub_app
-        conn.adapter :rack, rack_app.new if rack_app
-        if !stub_app && !rack_app
-          conn.adapter Faraday.default_adapter
-        end
-      end
     end
   end
 end
